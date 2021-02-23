527a1fc00f3bd200289209851b7b6351125a6ca8
Miquel Castro <castro-miquel@hotmail.com>
2021-02-23 23:53:27 +0100

    hago saltos de linea

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const text = "Ab;:.!baa";
 
 const esPalindromo = text => {
-  const textPreparadoCheck = text.toLowerCase().replace(/[.,:;()_?¿!¡-\s]/g, "").normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-  const textReversed = textPreparadoCheck.split("").reverse().join("");
+  const textPreparadoCheck = text.toLowerCase()
+    .replace(/[.,:;()_?¿!¡-\s]/g, "")
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+
+  const textReversed = textPreparadoCheck.split("")
+    .reverse()
+    .join("");
 
   return textPreparadoCheck === textReversed;
 };
