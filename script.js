ede22df897ab33210436e71e4511c1634880b498
Miquel Castro <castro-miquel@hotmail.com>
2021-02-23 23:50:56 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const text = "Ab;:.!baa";
+
+const esPalindromo = text => {
+  const textPreparadoCheck = text.toLowerCase().replace(/[.,:;()_?¿!¡-\s]/g, "").normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  const textReversed = textPreparadoCheck.split("").reverse().join("");
+
+  return textPreparadoCheck === textReversed;
+}
+
+console.log(esPalindromo(text));
